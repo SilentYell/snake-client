42dfa2a87e06e8c0e4f658426cc5bde01332e0c1
Nik Hixson <nikilobe@gmail.com>
2024-10-22 14:29:06 -0700

    added a client.js file and moved the connect function to it. modified the client file to export the connect function and modified the play.js file to import the connect function. Tested and still works

---
+++ client.js
@@ -0,0 +1,33 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: '10.255.255.254',
+    port: 50541,
+  });
+  
+  // when connected, prints a success message
+  conn.on('connect', () => {
+    console.log('Successfully connected to the server!');
+  });
+
+  // logs data received from the server
+  conn.on('data', (data) => {
+    console.log('Message from server:', data.toString());
+  });
+
+  // logs message when the server ends the connection
+  conn.on('end', () => {
+    console.log('Disconnected from server');
+  });
+
+  // logs an error message when a connection error occurs
+  conn.on('error', (err) => {
+    console.error('Connection error:', err.message);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,34 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: '10.255.255.254',
-    port: 50541,
-  });
-  
-  // when connected, prints a success message
-  conn.on('connect', () => {
-    console.log('Successfully connected to the server!');
-  });
-
-  // logs data received from the server
-  conn.on('data', (data) => {
-    console.log('Message from server:', data.toString());
-  });
-
-  // logs message when the server ends the connection
-  conn.on('end', () => {
-    console.log('Disconnected from server');
-  });
-
-  // logs an error message when a connection error occurs
-  conn.on('error', (err) => {
-    console.error('Connection error:', err.message);
-  });
-
-  return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
