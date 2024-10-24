aaa6a27083788fdcea8e0648c405272a957042bf
Nik Hixson <nikilobe@gmail.com>
2024-10-24 09:54:10 -0700

    added  if statements for other key presses 'w' 's' 'a' 'd' for directions the snake will move

--- input.js
+++ input.js
@@ -12,6 +12,16 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
+
+  if (key === 'w') {
+    console.log('Move: up');
+  } else if (key === 'a') {
+    console.log('Move: left');
+  } else if (key === 's') {
+    console.log('Move: down');
+  } else if (key === 'd') {
+    console.log('Move: right');
+  }
 };
 
 module.exports = {setupInput};
